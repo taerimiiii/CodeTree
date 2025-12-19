7a39bf98fc5b61ed04f055722f71a4160aece1d1
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:21:25 +0900

    Title: 두 실수의 곱, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        double a = 5.26;
+        double b = 8.27;
+
+        // 출력
+        System.out.printf("%.3f\n", a * b);
+    }
+}
+
