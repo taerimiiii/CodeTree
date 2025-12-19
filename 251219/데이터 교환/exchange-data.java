52eba1d7147220b1cc648af19ddb4386bfdbb651
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:22:59 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 5, b = 6, c = 7;
+
+        // 교체
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
+
+        // 출력
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
