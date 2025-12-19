bc9d77ac7bfde17e7fc8b68429463fcbd5647e00
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:22:07 +0900

    Title: 문자 변경하기, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/문자 변경하기/change-charater.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        char a = 'C';
+
+        // 값 변경
+        a = 'T';
+
+        // 출력
+        System.out.println(a);
+    }
+}
+
