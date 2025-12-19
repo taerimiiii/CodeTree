3a935d54101d813ddace91fe6708eb7ee7c27ff4
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:22:41 +0900

    Title: 정수 복사, Time: 81ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 3, b = 4;
+
+        // 값 변경
+        b = a;
+
+        // 출력
+        System.out.println(a + " " + b);
+        System.out.println(a * b);
+    }
+}
