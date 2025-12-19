8672f45f561ee9f7539e6850bc2ab7c86e14476b
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:14:07 +0900

    Title: 정수 선언하고 뺄셈, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 97, b = 13;
+        System.out.print(a + " - " + b + " = " + (a - b));
+    }
+}
