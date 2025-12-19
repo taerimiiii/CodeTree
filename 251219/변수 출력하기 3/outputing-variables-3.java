35c3b4dc62b5333057fe005a1a6cf9090ea5ec72
K. Taerim <shipton0201@gmail.com>
2025-12-19 18:16:49 +0900

    Title: 변수 출력하기 3, Time: 84ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251219/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int a = 1, b = 2;
+        char c = 'C';
+
+        // 출력
+        System.out.println(a + "->" + b + "->" + c);
+    }
+}
+
