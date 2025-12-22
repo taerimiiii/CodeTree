25a744b87e4bf028e7ec1b892564b13a76193d2d
K. Taerim <shipton0201@gmail.com>
2025-12-22 21:59:39 +0900

    Title: 합과 차의 나눗셈, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%.2f\n", (double)(a + b) / (a - b));
+    }
+}
+
