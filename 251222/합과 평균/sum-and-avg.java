1c852875cf68ba30a76aa4409b94ad0cf5bcf298
K. Taerim <shipton0201@gmail.com>
2025-12-22 22:00:16 +0900

    Title: 합과 평균, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/합과 평균/sum-and-avg.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int a, b;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // 출력
+        System.out.printf("%d %.1f\n", a + b, (double)(a + b) / 2);
+    }
+}
+
