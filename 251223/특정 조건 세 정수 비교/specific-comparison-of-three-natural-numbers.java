d2172f1edd093416ce5ca2fdec4990c97e8da066
Taerim <shipton0201@gmail.com>
2025-12-23 19:02:06 +0900

    Title: 특정 조건 세 정수 비교, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251223/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int a, b, c;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        
+        if(a <= b && a <= c)
+            System.out.print("1 ");
+        else
+            System.out.print("0 ");
+
+        if(a == b && b == c)
+            System.out.println("1");
+        else
+            System.out.println("0");
+    }
+}
