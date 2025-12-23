0e5d99b4e941780a8ef2596dbe8fb9d6beff5608
Taerim <shipton0201@gmail.com>
2025-12-23 19:07:08 +0900

    Title: 일 수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251223/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int n;
+        
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        if(n == 2)
+            System.out.println("28");
+        else if(n <= 7) {
+            if(n % 2 == 1)
+                System.out.println("31");
+            else
+                System.out.println("30");
+        }
+        else {
+            if(n % 2 == 0)
+                System.out.println("31");
+            else
+                System.out.println("30");
+        }
+    }
+}
