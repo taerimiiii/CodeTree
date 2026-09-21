ce5129bf2f0529e4f12379157db9cb3a5ce4d620
Taerim <shipton0201@gmail.com>
2026-09-21 15:47:59 +0900

    [Passed] 윤년인가 (51ms, 16MB)

--- 260921/윤년인가/is-leap-year.py
+++ 260921/윤년인가/is-leap-year.py
@@ -1,7 +1,17 @@
-y = int(input())
+# y = int(input())
 
-if y % 4 == 0 and ((y % 100 != 0) or (y % 400 == 0)):
-    print("true")
+# if y % 4 == 0 and ((y % 100 != 0) or (y % 400 == 0)):
+#     print("true")
+# else:
+#     print("윤년이 아니다")
+
+
+y=int(input())
+
+if y % 4 == 0 :
+    if y % 100 == 0 and y % 400 != 0 :
+        print('false')
+    else :
+        print('true')
 else:
-    print("false")
-
+    print('false')
